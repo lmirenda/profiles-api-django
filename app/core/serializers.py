39b2b768aebdd669719e65267c98c5540b210aad
lmirenda <lucamateomirenda@gmail.com>
2022-07-01 15:17:25 -0300

    Update profile item view permissions

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.relations import PrimaryKeyRelatedField
 
 from core import models
 
--- app/core/views.py
+++ app/core/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from core import serializers, models, permissions
 
@@ -123,7 +123,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (
         permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated,
     )
 
     def perform_create(self, serializer):
